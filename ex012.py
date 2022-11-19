98efa210b9036690d4c5bcdea235c981de2c25f1
Juliana Rodrigues Perillo <117007446+Juliana-RP@users.noreply.github.com>
2022-11-19 15:46:49 -0300

    Delete ex012.py

--- ex012.py
+++
@@ -1,6 +0,0 @@
-#Faça um algoritmo que leia o preço de um produto
-# e mostre seu novo preço, com 5% de desconto
-
-preco = float(input('Qual o preço do produto? R$ '))
-novo_preco = preco - (preco * 5 / 100)
-print('O produto custava R${} e com desconto de 5% está custando R${}'.format(preco, novo_preco))
