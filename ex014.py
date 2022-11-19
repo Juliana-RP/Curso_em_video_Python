238656bdd539dfb85c32807369826bcdee7df8f3
Juliana Rodrigues Perillo <117007446+Juliana-RP@users.noreply.github.com>
2022-11-19 15:47:03 -0300

    Delete ex014.py

--- ex014.py
+++
@@ -1,6 +0,0 @@
-# Escreva um programa que converta uma temperatura digitando
-# em graus Celsius e converta para graus Fahrenheit.
-
-c = float(input('A temperatura em °C é: '))
-f = ((9 * c) / 5) + 32
-print('A temperatura de {}ºC corresponde a {}°F.'.format(c, f))
